31bc544cc6db13b73a6748fce3b1556cd2cd01b4
Alex Li <alexlioralexli@gmail.com>
2022-04-27 16:38:54 -0400

    wandb website now reflects most recent metrics

--- src/dowel/wandb_output.py
+++ src/dowel/wandb_output.py
@@ -1,8 +1,6 @@
 """A `dowel.logger.LogOutput` for wandb.
 It receives the input data stream from `dowel.logger`, then logs it to the Weights and Biases online dashboard
 """
-import functools
-import numpy as np
 import wandb
 from dowel.logger import LoggerWarning, LogOutput
 from dowel.tabular_input import TabularInput
@@ -28,26 +26,27 @@ class WandbOutput(LogOutput):
             prefix(str): Not used.
         """
         if isinstance(data, TabularInput):
-            self._waiting_for_dump.append(
-                functools.partial(self._record_tabular, data))
+            self._waiting_for_dump.append(data)
         else:
             raise ValueError('Unacceptable type.')
 
-    def _record_tabular(self, data, step):
-        for key, value in data.as_dict.items():
-            self._record_kv(key, value, step)
-            data.mark(key)
+    def _record_tabular(self, data_lst):
+        all_data = {}
+        total_size = 0
+        for data in data_lst:
+            all_data.update(data.as_dict)
+            total_size += len(data.as_dict)
+            for key, value in data.as_dict.items():
+                data.mark(key)
 
-    def _record_kv(self, key, value, step):
-        if isinstance(value, np.ScalarType):
-            wandb.log({key: value}, step=step)
-        else:
-            raise NotImplementedError
+        # check that the keys are all disjoint
+        assert len(all_data) == total_size
+        wandb.log(all_data)
+
 
     def dump(self, step=None):
         """Flush summary writer to disk."""
         # Log the tabular inputs, now that we have a step
-        for p in self._waiting_for_dump:
-            p(step or self._default_step)
+        self._record_tabular(self._waiting_for_dump)
         self._waiting_for_dump.clear()
         self._default_step += 1
